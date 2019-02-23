9cc05a8a5ff063e4e1a19b943e2fd1e8b92a5333
unknown <mashkal2000@gmail.com>
2019-02-23 18:30:43 -0500

    add debug mode

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ def transcribe(image_path, mode: int, drive=None):
     img = ImageEnhance.Contrast(img)
     img = img.enhance(2)
     img = img.convert('1')
-    img.show()
 
     text = pytesseract.image_to_string(img)
-    # Modes: 0 -> print; 1 -> save local docx; 2 -> upload to drive
+    # Modes: 0 -> print; 1 -> save local docx; 2 -> upload to drive 3 -> debug
     if mode == 0:
         print(text)
+        img.show()
 
     if mode == 1:
         with open('test data/document.docx', 'w') as file:
@@ -28,7 +28,16 @@ def transcribe(image_path, mode: int, drive=None):
         drive_file = drive.CreateFile({'title': 'Image Text'})
         drive_file.SetContentString(text)
         drive_file.Upload()
+    
+    if mode == 3: # debug
+        # Get bounding box estimates
+        print(pytesseract.image_to_boxes(img))
 
+        # Get verbose data including boxes, confidences, line and page numbers
+        print(pytesseract.image_to_data(img))
+
+        # Get information about orientation and script detection
+        print(pytesseract.image_to_osd(img))
 
 def main():
     # Check for alternate path string
