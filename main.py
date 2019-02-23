135255bcaf2b160fc5c463de3560393168ed73f4
Rob Lenovo <robertzengheliu@gmail.com>
2019-02-23 04:11:04 -0500

    more resource efficient

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
 
-im = Image.open("label_meme.jpg") # the second one
-im = im.filter(ImageFilter.MedianFilter())
-enhancer = ImageEnhance.Contrast(im)
-im = enhancer.enhance(2)
-im = im.convert('1')
-im.save('label_meme2.jpg')
-text = pytesseract.image_to_string(Image.open('label_meme2.jpg'))
+with Image.open("label_meme.jpg") as img:
+    filtered_img = img.filter(ImageFilter.MedianFilter())
+enhancer = ImageEnhance.Contrast(filtered_img)
+img = enhancer.enhance(2)
+img = img.convert('1')
+img.save('label_meme2.jpg')
+with Image.open('label_meme2.jpg') as img:
+    text = pytesseract.image_to_string(img)
 print(text)
