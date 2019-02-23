0ac8eaf6330b80de7bc58cbe1b5de0166f63a691
Rob Lenovo <robertzengheliu@gmail.com>
2019-02-23 13:45:37 -0500

    organized functions

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from PIL import Image, ImageEnhance, ImageFilter
 from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
 
-
+# Google api permissions authentication
 g_auth = GoogleAuth()
 drive = GoogleDrive(g_auth)
 
+# Check for alternate path string
 try:
     with open('tesseract_cmd') as path_file:
         path = path_file.read()
@@ -15,20 +16,31 @@ except FileNotFoundError:
     print("No alternate file found")
 
 
-with Image.open('test data/page.png') as img:
-    filtered_img = img.filter(ImageFilter.MedianFilter())
-enhancer = ImageEnhance.Contrast(filtered_img)
-img = enhancer.enhance(2)
-img = img.convert('1')
-img.save('test data/page_2.png')
-with Image.open('test data/page_2.png') as img:
+# Transcribing image to text
+def transcribe(image_path, mode: int):
+    with Image.open(image_path) as img_file:
+        img = img_file.filter(ImageFilter.MedianFilter())
+    img = ImageEnhance.Contrast(img)
+    img = img.enhance(2)
+    img = img.convert('1')
     text = pytesseract.image_to_string(img)
-print(text)
+# Modes: 0 -> print; 1 -> save local docx; 2 -> upload to drive
+    if mode == 0:
+        print(text)
+
+    if mode == 1:
+        with open('test data/document.docx', 'w') as file:
+            file.write(text)
+
+    if mode == 2:
+        drive_file = drive.CreateFile({'title': 'Image Text'})
+        drive_file.SetContentString(text)
+        drive_file.Upload()
 
 
-with open('test data/document.docx', 'w') as file:
-    file.write(text)
+def __main__():
+    transcribe('test data/page.png', 2)
 
-drive_file = drive.CreateFile({'title': 'Image Text'})
-drive_file.SetContentString(text)
-drive_file.Upload()
+
+if __name__ == __main__:
+    __main__()
