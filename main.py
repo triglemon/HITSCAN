3e388a0436117095acc07b681ea8451cf9fe5626
Rob Lenovo <robertzengheliu@gmail.com>
2019-02-23 04:23:46 -0500

    changed tesseract.exe path

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
 
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Robert\AppData\Local\Tesseract-OCR\tesseract.exe'
 with Image.open("label_meme.jpg") as img:
     filtered_img = img.filter(ImageFilter.MedianFilter())
 enhancer = ImageEnhance.Contrast(filtered_img)
