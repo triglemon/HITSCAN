c8fafe857dbee4de1d528acbab8aa271394ba04c
Rob Lenovo <robertzengheliu@gmail.com>
2019-02-23 02:41:43 -0500

    image input to black and white output

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import pytesseract
+from PIL import Image, ImageEnhance, ImageFilter
+
+im = Image.open("label_meme.jpg") # the second one
+im = im.filter(ImageFilter.MedianFilter())
+enhancer = ImageEnhance.Contrast(im)
+im = enhancer.enhance(2)
+im = im.convert('1')
+im.save('label_meme2.jpg')
+text = pytesseract.image_to_string(Image.open('label_meme2.jpg'))
+print(text)
