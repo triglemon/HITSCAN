035edabad8859df57d8044ea950804ce6143eb20
Rob Lenovo <robertzengheliu@gmail.com>
2019-02-23 10:13:07 -0500

    different input img

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from PIL import Image, ImageEnhance, ImageFilter
 
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Robert\AppData\Local\Tesseract-OCR\tesseract.exe'
-with Image.open("label_meme.jpg") as img:
+with Image.open('page.png') as img:
     filtered_img = img.filter(ImageFilter.MedianFilter())
 enhancer = ImageEnhance.Contrast(filtered_img)
 img = enhancer.enhance(2)
 img = img.convert('1')
-img.save('label_meme2.jpg')
-with Image.open('label_meme2.jpg') as img:
+img.save('page_2.png')
+with Image.open('page_2.png') as img:
     text = pytesseract.image_to_string(img)
 print(text)
