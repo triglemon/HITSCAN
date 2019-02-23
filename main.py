b9e558bd3fc4b0ee3e106a84c99fe4e6890c49c3
Rob Lenovo <robertzengheliu@gmail.com>
2019-02-23 13:36:07 -0500

    organized input/output data; added alternate tesseract.exe path checker

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
-from docx import document
 from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
 
@@ -8,19 +7,26 @@ from pydrive.auth import GoogleAuth
 g_auth = GoogleAuth()
 drive = GoogleDrive(g_auth)
 
-# pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Robert\AppData\Local\Tesseract-OCR\tesseract.exe'
-with Image.open('page.png') as img:
+try:
+    with open('tesseract_cmd') as path_file:
+        path = path_file.read()
+        pytesseract.pytesseract.tesseract_cmd = path
+except FileNotFoundError:
+    print("No alternate file found")
+
+
+with Image.open('test data/page.png') as img:
     filtered_img = img.filter(ImageFilter.MedianFilter())
 enhancer = ImageEnhance.Contrast(filtered_img)
 img = enhancer.enhance(2)
 img = img.convert('1')
-img.save('page_2.png')
-with Image.open('page_2.png') as img:
+img.save('test data/page_2.png')
+with Image.open('test data/page_2.png') as img:
     text = pytesseract.image_to_string(img)
 print(text)
 
 
-with open('document.docx', 'w') as file:
+with open('test data/document.docx', 'w') as file:
     file.write(text)
 
 drive_file = drive.CreateFile({'title': 'Image Text'})
